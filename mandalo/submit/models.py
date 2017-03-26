dc192aa61d56364396547f497d1ff9f890428636
Alex Gunter <akgunter@go.olemiss.edu>
2017-03-25 19:11:09 -0500

    Fixed a typo

--- mandalo/submit/models.py
+++ mandalo/submit/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Assigment(models.Model):
+class Assignment(models.Model):
     name = models.CharField(max_length=20)
     language = models.CharField(max_length=20)
     created_date = models.DateTimeField()
