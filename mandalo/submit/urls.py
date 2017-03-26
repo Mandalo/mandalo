0fdf273c60d9a7cc009b95f27db92ee23d0359ae
Javier A. Suarez Rivas <jsuarez@go.olemiss.edu>
2017-03-25 20:41:08 -0500

    feat: added upload page

--- mandalo/submit/urls.py
+++ mandalo/submit/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^view_assign/', views.view_assign, name='view_assign')
+    url(r'^view_assign/', views.view_assign, name='view_assign'),
+    url(r'^submit/', views.upload, name='upload')
 ]
